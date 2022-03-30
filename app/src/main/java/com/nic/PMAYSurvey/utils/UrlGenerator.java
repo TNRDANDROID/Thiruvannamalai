7f794a9049facd774e6bf7a9a8c75ecad4418ffa
NICInternship <workingprojects22@gmail.com>
2022-03-30 14:45:36 +0530

    Initial commit

---
+++ app/src/main/java/com/nic/PMAYSurvey/utils/UrlGenerator.java
@@ -0,0 +1,32 @@
+package com.nic.PMAYSurvey.utils;
+
+
+import com.nic.PMAYSurvey.R;
+import com.nic.PMAYSurvey.application.NICApplication;
+
+/**
+ * Created by Achanthi Sundar  on 21/03/16.
+ */
+public class UrlGenerator {
+
+
+
+    public static String getLoginUrl() {
+        return NICApplication.getAppString(R.string.LOGIN_URL);
+    }
+
+    public static String getServicesListUrl() {
+        return NICApplication.getAppString(R.string.BASE_SERVICES_URL);
+    }
+
+    public static String getPMAYListUrl() {
+        return NICApplication.getAppString(R.string.APP_MAIN_SERVICES_URL);
+    }
+
+    public static String getTnrdHostName() {
+        return NICApplication.getAppString(R.string.TNRD_HOST_NAME);
+    }
+
+
+
+}
