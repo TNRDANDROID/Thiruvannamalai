d497d7f7b8c71e439b5808cb152e9f54deef2724
AchanthiSundar <tnrdmobile@gmail.com>
2022-11-14 11:45:36 +0530

    initial commit

--- app/src/main/java/com/nic/Thiruvannamalai/activity/Dashboard.java
+++ app/src/main/java/com/nic/Thiruvannamalai/activity/Dashboard.java
@@ -36,7 +36,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class Dashboard extends AppCompatActivity implements Api.ServerResponseListener, View.OnClickListener, MyDialog.myOnClickListener {
+public class Dashboard extends AppCompatActivity implements View.OnClickListener, MyDialog.myOnClickListener {
     private DashboardBinding dashboardBinding;
     private PrefManager prefManager;
 
@@ -52,22 +52,15 @@ public class Dashboard extends AppCompatActivity implements Api.ServerResponseLi
         dashboardBinding.setActivity(this);
         prefManager = new PrefManager(this);
 
-        dashboardBinding.menuIcon.setOnClickListener(new View.OnClickListener() {
+        dashboardBinding.detailsEntry.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                openMenuDrawer();
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this,DetailsEnterScreen.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in,R.anim.slide_out);
             }
         });
-        getTaxTypeList();
 
-    }
-
-    public void openMenuDrawer(){
-        if(dashboardBinding.drawerLayout.isDrawerOpen(Gravity.LEFT)){
-            dashboardBinding.drawerLayout.closeDrawer(Gravity.LEFT);
-        }else{
-            dashboardBinding.drawerLayout.openDrawer(Gravity.LEFT);
-        }
     }
 
     @Override
@@ -89,45 +82,6 @@ public class Dashboard extends AppCompatActivity implements Api.ServerResponseLi
         super.onResume();
 
     }
-    public void getTaxTypeList() {
-        try {
-            new ApiService(this).makeJSONObjectRequest("TaxTypeList", Api.Method.POST, UrlGenerator.getServicesListUrl(), taxTypeListJsonParams(), "not cache", this);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-    public JSONObject taxTypeListJsonParams() throws JSONException {
-        JSONObject dataSet = new JSONObject();
-        dataSet.put(AppConstant.KEY_SERVICE_ID, "TaxTypeList");
-        Log.d("taxTypeList", "" + dataSet);
-        return dataSet;
-    }
-
-    @Override
-    public void OnMyResponse(ServerResponse serverResponse) {
-        try {
-            JSONObject responseObj = serverResponse.getJsonResponse();
-            String urlType = serverResponse.getApi();
-            String status;
-            String response;
-
-            if ("TaxTypeList".equals(urlType) && responseObj != null) {
-                if (responseObj.getString("STATUS").equalsIgnoreCase("SUCCESS") && responseObj.getString("RESPONSE").equalsIgnoreCase("SUCCESS")) {
-                    new Insert_TaxCollection().execute(responseObj);
-                }
-                Log.d("TaxCollection", "" + responseObj);
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void OnError(VolleyError error) {
-
-    }
-
 
     public void closeApplication() {
         new MyDialog(this).exitDialog(this, "Are you sure you want to Logout?", "Logout");
@@ -157,72 +111,6 @@ public class Dashboard extends AppCompatActivity implements Api.ServerResponseLi
             startActivity(intent);
             finish();
             overridePendingTransition(R.anim.slide_enter, R.anim.slide_exit);
-        }
-    }
-
-    @SuppressLint("StaticFieldLeak")
-    public class Insert_TaxCollection extends AsyncTask<JSONObject, Void, ArrayList<PublicTax>> {
-
-        @Override
-        protected ArrayList<PublicTax> doInBackground(JSONObject... params) {
-
-            if (params.length > 0) {
-                taxTypeList = new ArrayList<>();
-                JSONArray jsonArray = new JSONArray();
-
-                try {
-                    jsonArray = params[0].getJSONArray("DATA");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                for (int i = 0; i < jsonArray.length(); i++) {
-
-                    try {
-                        PublicTax tax = new PublicTax();
-                        tax.setTaxtypeid(jsonArray.getJSONObject(i).getString("taxtypeid"));
-                        tax.setTaxtypedesc_en(jsonArray.getJSONObject(i).getString("taxtypedesc_en"));
-
-                        taxTypeList.add(tax);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-
-
-
-            }
-
-
-            return taxTypeList;
-
-
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<PublicTax> list) {
-            super.onPostExecute(list);
-
-            ArrayList<Integer> taxImageList = new ArrayList<>();
-            taxImageList.add(R.drawable.property_tax);
-            taxImageList.add(R.drawable.water_tap);
-            taxImageList.add(R.drawable.professional_tax);
-            taxImageList.add(R.drawable.non_tax);
-            taxImageList.add(R.drawable.trade_licence_tax);
-//            dashboardBinding.recycler.setLayoutManager(new GridLayoutManager(getApplicationContext(),3));
-            dashboardBinding.recycler.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false));
-
-            if(list.size()>0){
-                dashboardBinding.noDataFound.setVisibility(View.GONE);
-                dashboardBinding.recycler.setVisibility(View.VISIBLE);
-                taxListAdapter = new TaxTypeListAdapter(Dashboard.this,list,taxImageList);
-                dashboardBinding.recycler.setAdapter(taxListAdapter);
-            }else {
-                dashboardBinding.noDataFound.setVisibility(View.VISIBLE);
-                dashboardBinding.recycler.setVisibility(View.GONE);
-            }
-
-
         }
     }
 
